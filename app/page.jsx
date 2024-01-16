faf4e61d95cb79f22b7e256f468f4641b05dce9b
DebrajLogic <debrajdey9696@gmail.com>
2024-01-16 05:11:00 -0500

    improved visuals

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,12 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover & Share <br />
-        <span className="orange_gradient">AI-Powered Prompts</span>
+        Your Personal <br />
+        <span className="orange_gradient">AI Prompts Repository</span>
       </h1>
       <p className="desc text-center">
-        Prompt-Store is an AI prompt store to make your life easier
+        Prompt-Store is your On-the-Go Solution for Storing and Retrieving All
+        Your Useful Prompts!
       </p>
       <Feed />
     </section>
